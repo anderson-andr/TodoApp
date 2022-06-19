7db8cdd2713bb650ff0dbad7575730d7de77120e
anderson-andr <55621198+anderson-andr@users.noreply.github.com>
2022-06-18 23:01:36 -0300

    initial commit

---
+++ app/src/main/java/TodoApp/App.java
@@ -0,0 +1,44 @@
+
+package TodoApp;
+
+import controller.ProjectController;
+import controller.TaskController;
+import java.sql.Connection;
+import java.util.List;
+import model.Project;
+import model.Task;
+import util.ConnectionFactory;
+
+
+public class App {
+   
+    public static void main(String[] args) {
+       
+    /* ProjectController projectController = new ProjectController ();
+       
+   
+      
+     
+     
+       task.setIdProject(2);
+       task.setName("Projeto");
+       task.setDescription("Description");
+       
+       taskController.save(task);
+      
+       project.setId(1);
+       project.setName("Novo nome");
+       project.setDescription("Nova descrição");
+       
+       projectController.update(project);
+       
+     
+        List<Project> projects = projectController.getAll();
+       
+        System.out.println("Total de Projetos = " + projects.size());*/
+  
+     
+        
+     
+    }
+}
